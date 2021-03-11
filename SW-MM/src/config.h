9bbfcd79d3009fe06b4d0bdba01a2b9bc31b101d
Konstantin Láska <konstantin.laska@icloud.com>
2021-03-11 12:35:51 +0100

    Project separated

--- SW-MM/src/config.h
+++
@@ -1,6 +0,0 @@
-// Wifi settings
-
-char ssid[] = "yourWiFiSSIDname";   // your network SSID (name)
-char pass[] = "yourWiFipassword";   // your network password
-
-const char* api_key = "yourThingspeakAPIKey";   //Thingspeak API Key
