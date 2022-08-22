3d6ff2afcf2eda3b84ca11d797962f6afcd3a492
Konstantin Láska <konstantin.laska@icloud.com>
2022-08-22 13:55:44 +0200

    ADC update

--- SW/Meteo_Mini_ADC/Meteo_Mini_ADC.ino
+++ SW/Meteo_Mini_ADC/Meteo_Mini_ADC.ino
@@ -1,33 +1,37 @@
-#include "esp_adc_cal.h"
+/*
+* This example code is used for LaskaKit Meteo Mini board 
+*
+* Meteo Mini ESP board reads voltage on Battery and Solar panel 
+* and sends every 2 seconds through UART
+* 
+*
+* Made by (c) laskakit.cz 2022
+* 
+*/
 
-#define ADC_PIN     0
+#include <SPI.h>
+#include <Wire.h>
+#include <ESP32AnalogRead.h>    // https://github.com/madhephaestus/ESP32AnalogRead
+
+ESP32AnalogRead adc;
+#define ADCpin 0
+#define DeviderRatio 1.7693877551  // Voltage devider ratio on ADC pin 1MOhm + 1.3MOhm
 
 void setup() {
+    
   Serial.begin(115200);
+  delay(100); // let serial console settle
+  
+  // setting ADC
+  adc.attach(ADCpin);
 }
 
 void loop() {
-  float unadjusted_voltage = (analogRead(ADC_PIN) / 4095.0 * 4.423);
-  float adjusted_voltage   = ReadVoltage(ADC_PIN);
-  
-  Serial.println("      Input Voltage = 4.026 v");
-  Serial.println("------------------------------");
-  Serial.println("   Adjusted Voltage = " + String(adjusted_voltage, 3) + "v  " + String(adjusted_voltage / 4.026 * 100 - 100) + "% error");
-  Serial.println("Un-adjusted Voltage = " + String(unadjusted_voltage, 3) + "v  " + String(unadjusted_voltage / 4.026 * 100 - 100) + "% error");
-  Serial.println();
-  delay(5000);
-}
 
-float ReadVoltage(byte ADC_Pin) {
-  float calibration  = 1.000; // Adjust for ultimate accuracy when input is measured using an accurate DVM, if reading too high then use e.g. 0.99, too low use 1.01
-  float vref = 1100;
-  esp_adc_cal_characteristics_t adc_chars;
-  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
-  vref = adc_chars.vref; // Obtain the device ADC reference voltage
-  return (analogRead(ADC_PIN) / 4095.0) * 4.423 * (1100 / vref) * calibration;  // ESP by design reference voltage in mV
-}
+  float bat_voltage = adc.readVoltage() * DeviderRatio;
+  Serial.print("Battery Voltage = " );
+  Serial.print(bat_voltage);
+  Serial.println("V");
 
-// The esp_adc_cal/include/esp_adc_cal.h API provides functions to correct for differences
-// in measured voltages caused by variation of ADC reference voltages (Vref) between chips.
-// Per design the ADC reference voltage is 1100 mV, however the true reference voltage can
-// range from 1000 mV to 1200 mV amongst different ESP32's
+  delay(2000);
+}
